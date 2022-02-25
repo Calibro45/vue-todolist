419cd3322345b6b94386a22131d6ae1f3be6035e
andry16-10-91@hotmail.it <andry16-10-91@hotmail.it>
2022-02-25 15:07:34 +0100

    creato oggetti

--- js/script.js
+++ js/script.js
@@ -3,7 +3,31 @@ const app = new Vue ({
     el: '#app',
 
     data: {
-
+        todoList: [
+            {
+                text: 'Fare la spesa',
+                done: false,
+            },
+            {
+                text: 'Rifare la stanza',
+                done: false,
+            },
+            {
+                text: 'Giocare alla play',
+                done: false,
+            },
+            {
+                text: 'Suonare la chitarra',
+                done: false,
+            },
+            {
+                text: 'Studiare',
+                done: false,
+            },{
+                text: 'Rilassarsi sul divano',
+                done: false,
+            },
+        ]
     },
 
 })
